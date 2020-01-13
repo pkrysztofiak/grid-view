a2733dc0b80386bf16fdef00905d31e99234efe7
p.krysztofiak <p.krysztofiak@pixel.com.pl>
2020-01-13 18:31:10 +0100

    Add hanging protocol panel

---
+++ src/main/java/pl/pkrysztofiak/gridview/model/hangingprotocol/panel/Behaviour.java
@@ -0,0 +1,32 @@
+package pl.pkrysztofiak.gridview.model.hangingprotocol.panel;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.rxjavafx.observables.JavaFxObservable;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import pl.pkrysztofiak.gridview.model.panels.PanelModel;
+
+public class Behaviour {
+
+public final PublishSubject<PanelModel> addPanelRequestPublishable = PublishSubject.create();
+    
+    private final ObservableList<PanelModel> panels = FXCollections.observableArrayList();
+    private final ObservableList<PanelModel> unmodifiablePanels = FXCollections.unmodifiableObservableList(panels);
+    private final Observable<PanelModel> panelAddedObservable = JavaFxObservable.additionsOf(panels);
+ 
+    {
+        addPanelRequestPublishable.delay(0, TimeUnit.SECONDS, Schedulers.single()).subscribe(this::onAddPanelRequest);
+    }
+    
+    public ObservableList<PanelModel> getPanels() {
+        return unmodifiablePanels;
+    }
+    
+    private void onAddPanelRequest(PanelModel panel) {
+        panels.add(panel);
+    }
+}
--- src/main/java/pl/pkrysztofiak/gridview/model/hangingprotocol/panel/HangingProtocolPanel.java
+++ src/main/java/pl/pkrysztofiak/gridview/model/hangingprotocol/panel/HangingProtocolPanel.java
@@ -1,5 +1,24 @@
 package pl.pkrysztofiak.gridview.model.hangingprotocol.panel;
 
+import java.util.stream.Stream;
+
+import javafx.collections.ObservableList;
+import pl.pkrysztofiak.gridview.model.hangingprotocol.Behaviour;
+import pl.pkrysztofiak.gridview.model.panels.PanelModel;
+
 public class HangingProtocolPanel {
 
+    private final Behaviour behaviour = new Behaviour();
+
+    public HangingProtocolPanel(PanelModel... panels) {
+        Stream.of(panels).forEach(behaviour.addPanelRequestPublishable::onNext);
+    }
+    
+    public void addPanel(PanelModel panel) {
+        behaviour.addPanelRequestPublishable.onNext(panel);
+    }
+    
+    public ObservableList<PanelModel> getPanels() {
+        return behaviour.getPanels();
+    }
 }
